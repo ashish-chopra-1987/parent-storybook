bca6d9c60c7187d6b9e79b89640cbaa9b8d465a8
Chopra, Ashish <ashish.chopra.1987@gmail.com>
2022-06-21 08:50:03 +0530

    Added files from another repository

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+module.exports = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack4",
+  },
+  refs: {
+    'design-system': {
+      title: "Storybook Design System",
+      url: "https://5ccbc373887ca40020446347-yldsqjoxzb.chromatic.com",
+      expanded: false // optional, true by default
+    },
+    'task': {
+      title: 'Sample 1 - Task A',
+      url: 'https://ashish-chopra-1987.github.io/childStorybook/',
+      expanded: false,
+    }
+  },
+};
