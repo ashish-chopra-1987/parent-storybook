98c281a9c31a86a5565a6567fbacdee012741d91
Chopra, Ashish <ashish.chopra.1987@gmail.com>
2022-06-22 17:40:06 +0530

    Updated main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     'child-storybook-metadata': {
       title: 'Child Storybook Example',
-      url: 'https://main--62b1b9ef50b3e0e9a6b8b90f.chromatic.com/',
+      url: 'https://62b1b9ef50b3e0e9a6b8b90f-hwmhdbjzfg.chromatic.com/',
       expanded: false,
     },
   },
